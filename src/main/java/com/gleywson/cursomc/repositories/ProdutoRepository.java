1d79694439628ba9791519a56f147e10e856c9bc
Gleywson Ribeiro <gleywson@gmail.com>
2020-01-27 14:27:16 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/gleywson/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.gleywson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gleywson.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
+	
+}
