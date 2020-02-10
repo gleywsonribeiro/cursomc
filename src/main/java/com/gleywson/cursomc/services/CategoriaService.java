4dd51f9a126ef0db445543b45595d0f8093aaccd
gleywson Ribeiro <gleywson@gmail.com>
2020-02-10 14:29:52 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gleywson/cursomc/services/CategoriaService.java
+++ src/main/java/com/gleywson/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.gleywson.cursomc.domain.Categoria;
 import com.gleywson.cursomc.repositories.CategoriaRepository;
+import com.gleywson.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
